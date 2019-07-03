e28856e1cb0dfbfe1605d83e68ad76f93dc733e4
Richard Pitts <richardepitts@gmail.com>
2019-07-03 21:19:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ function getFirstSelector(selector){
  function nestedTarget() {
    return document.getElementById('nested').querySelector('div.target')
  }
+ 
+ 
+ function increaseRankBy(n) {
+  const rankHigher = document.getElementById('app').querySelectorAll('ul.ranked-list')
+  
+  for (let i = 0; i < rankHigher.length; i++) {
+    rankHigher[i].innerHTML = ((parseInt(rankHigher[i].innerHTML)) + n)
+  }
+ }
